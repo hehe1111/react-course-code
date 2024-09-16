3a2bc69e4ab2fdf3c7c034bceb4dbe67065d2195
hehe11111 <2908749709@qq.com>
2024-09-16 13:18:55 +0800

    feat(react-play-ground-project): 添加行上移、行下移、字体缩放操作

--- react-playground-project/src/ReactPlayground/components/CodeEditor/Editor/index.tsx
+++ react-playground-project/src/ReactPlayground/components/CodeEditor/Editor/index.tsx
@@ -23,12 +23,44 @@ export default function Editor(props: Props) {
     } = props;
 
     const handleEditorMount: OnMount = (editor, monaco) => {
+        /** 获取 monaco editor 支持的全部 action */
+        // const actions = editor.getSupportedActions().map((a) => a.id);
+        // console.log('====', actions);
 
+        /** 代码格式化 */
         editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyJ, () => {
             editor.getAction('editor.action.formatDocument')?.run()
-            // let actions = editor.getSupportedActions().map((a) => a.id);
-            // console.log(actions);
         });
+
+        /** 行上移 */
+        editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.UpArrow, () => {
+            editor.getAction('editor.action.moveLinesUpAction')?.run()
+        })
+        /** 行下移 */
+        editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.DownArrow, () => {
+            editor.getAction('editor.action.moveLinesDownAction')?.run()
+        })
+
+        /** 字体缩放 */
+        const editorElement = editor.getDomNode()!
+        // Add event listener for wheel event
+        editorElement.addEventListener('wheel', event => {
+            if (event.ctrlKey) {
+                // Wheel up
+                if (event.deltaY < 0) {
+                    // Perform action for Ctrl + Wheel Up
+                    editor.getAction('editor.action.fontZoomIn')?.run()
+                }
+                // Wheel down
+                else if (event.deltaY > 0) {
+                    // Perform action for Ctrl + Wheel Down
+                    editor.getAction('editor.action.fontZoomOut')?.run()
+                }
+
+                // Prevent default behavior (optional, depending on your use case)
+                event.preventDefault()
+            }
+        })
 
         monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
             jsx: monaco.languages.typescript.JsxEmit.Preserve,
