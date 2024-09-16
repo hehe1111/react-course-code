8571af1150c2adb4d3816b661680ae1fbed5479b
hehe11111 <2908749709@qq.com>
2024-09-16 16:17:41 +0800

    style(react-playground-project): 添加注释

--- react-playground-project/src/ReactPlayground/components/Preview/compiler.ts
+++ react-playground-project/src/ReactPlayground/components/Preview/compiler.ts
@@ -3,6 +3,9 @@ import { File, Files } from '../../PlaygroundContext'
 import { ENTRY_FILE_NAME } from '../../files'
 import { PluginObj } from '@babel/core';
 
+/**
+ * 替 .jsx 和 .tsx 文件自动引入 react
+ */
 export const beforeTransformCode = (filename: string, code: string) => {
     let _code = code
     const regexReact = /import\s+React/g
@@ -12,6 +15,16 @@ export const beforeTransformCode = (filename: string, code: string) => {
     return _code
 }
 
+/**
+ * 1. 替 .jsx 和 .tsx 文件自动引入 react
+ * 2. 把 json 、 css 转换为 js
+ *   2.1 json 直接 export 即可
+ *   2.2 css 需要使用 style 标签插入页面中
+ * 3. 处理 import 的 source： blob url。（注意：js 在转为 blob url 之前，需要先用 babel 编译，再把编译后的 js 转为 blob url）
+ *   3.1 相对路径
+ *   3.2 第三方模块
+ * 4. babel 编译
+ */
 export const babelTransform = (filename: string, code: string, files: Files) => {
     let _code = beforeTransformCode(filename, code);
     let result = ''
@@ -28,6 +41,11 @@ export const babelTransform = (filename: string, code: string, files: Files) => 
     return result
 }
 
+/**
+ * 万一输入的是 ./App 这种路径，也要能查找到对应的 App.tsx 模块：
+ * 如果去掉 ./ 之后，剩下的不包含 . 比如 ./App 这种，那就要补全 App 为 App.tsx 等。
+ * 过滤下 files 里的 js、jsx、ts、tsx 文件，如果包含这个名字的模块，那就按照补全后的模块名来查找 file。
+ */
 const getModuleFile = (files: Files, modulePath: string) => {
     let moduleName = modulePath.split('./').pop() || ''
     if (!moduleName.includes('.')) {
